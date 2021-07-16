faa0f8dfbfae8edcd6582f32afd1e0bb29dbf35f
Nicolás <euclidesnoeuclides@gmail.com>
2021-07-16 15:14:48 -0500

    basic set up twitter lite alike app with rails.

---
+++ README.md
@@ -0,0 +1,6 @@
+## Cheep
+
+Twitter but cheaper
+
+* Rails 6.1.4
+* ruby 3.0.1p64
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,18 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all.order(created_at: :desc)
+    @post = Post.new
+  end
+
+  # POST /posts or /posts.json
+  def create
+    @post = Post.create(post_params)
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
+  end
+end
